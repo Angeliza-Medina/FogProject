58d99e9bd29f6fd8e55a01a3e8f388a544496552
Philippe16 <philippe.suenson@hotmail.com>
2021-12-05 19:11:05 +0100

    add small amount of javascript to filters

---
+++ fog_web/src/main/webapp/assets/js/ccpiList.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+
+const allFilter_element = document.querySelector("#allFilter");
+const pendingFilter_element = document.querySelector("#pendingFilter");
+const inProgressFilter_element = document.querySelector("#inProgressFilter");
+const completedFilter_element = document.querySelector("#completedFilter");
+const cancelledFilter_element = document.querySelector("#cancelledFilter");
+const filterBtn_elements = document.querySelectorAll(".filterBtn");
+
+const currentLocation = window.location.href;
+const url = new URL(currentLocation);
+const filterParam = url.searchParams.get("filterType");
+
+// Reset all button background-color and its icon color
+filterBtn_elements.forEach(element => {
+    element.style.backgroundColor = "var(--darkBlue)";{}
+
+    if(element.querySelector(".filterBtn_icon") !== null){
+        element.querySelector(".filterBtn_icon").style.color = "var(--orange)";
+    }
+});
+
+// Change the clicked button's background-color and icon color
+if(filterParam === null || filterParam === "all"){
+    allFilter_element.style.backgroundColor = "var(--orange)";
+}else if(filterParam === "pending"){
+    pendingFilter_element.style.backgroundColor = "var(--orange)";
+    pendingFilter_element.querySelector(".filterBtn_icon").style.color = "var(--darkBlue)";
+}else if(filterParam === "inProgress"){
+    inProgressFilter_element.style.backgroundColor = "var(--orange)";
+    inProgressFilter_element.querySelector(".filterBtn_icon").style.color = "var(--darkBlue)";
+}else if(filterParam === "completed"){
+    completedFilter_element.style.backgroundColor = "var(--orange)";
+    completedFilter_element.querySelector(".filterBtn_icon").style.color = "var(--darkBlue)";
+}else if(filterParam === "cancelled"){
+    cancelledFilter_element.style.backgroundColor = "var(--orange)";
+    cancelledFilter_element.querySelector(".filterBtn_icon").style.color = "var(--darkBlue)";
+}
