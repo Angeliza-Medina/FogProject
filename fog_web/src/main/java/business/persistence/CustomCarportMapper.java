1bd288b637b0323e0b2f4de8f0246d941d7a8e6d
Angeliza Medina <78863493+Angeliza-Medina@users.noreply.github.com>
2021-12-20 16:56:46 +0100

    Fix minor typos

--- fog_web/src/main/java/business/persistence/CustomCarportMapper.java
+++ fog_web/src/main/java/business/persistence/CustomCarportMapper.java
@@ -281,40 +281,28 @@ public class CustomCarportMapper {
         }
     }
 
-
-
-    public CustomCarportInquiry getSearchInquiryById(String searchInput) throws UserException{
-        CustomCarportInquiry customCarportInquiry = null;
+    public ArrayList<CustomCarportInquiry> getSearchInquiryById(int searchInput) throws UserException{
+        ArrayList<CustomCarportInquiry> inquiries = new ArrayList<>();
 
         try (Connection connection = database.connect()) {
             String sql =
-                   " SELECT\n" +
-                   "    custom_carport_inquiry_id AS inquiry_id,\n" +
-                   "    inquiryDate,\n" +
-                   "    ccp_inquiry_statuses.status AS status,\n" +
-                   "    firstName, lastName, email, phoneNum, address, postalcode, city, note,\n" +
-                   "    ccp.ccp_id AS ccp_id,\n" +
-                   "    ccp.fk_ccpWidth AS ccpWidth,\n" +
-                   "    ccp.fk_ccpLength AS ccpLength,\n" +
-                   "    ccp.fk_ccpHeight AS ccpHeight,\n" +
-                   "    ccp.middlePillar AS middlePillar,\n" +
-                   "    ccp.fk_rafterSpacing AS rafterSpacing,\n" +
-                   "    ccp.fk_ccpRoofType_id AS roofType_id,\n" +
-                   "    ccp.fk_ccpRoofAngle AS roofAngle,\n" +
-                   "    ccp.fk_ccpRoofMaterial_id AS roofMaterial_id,\n" +
-                   "    ccp.price,\n" +
-                   "    ccp.fk_cts_id AS cts_id,\n" +
-                   "    cts.fk_ctsWidth AS ctsWidth,\n" +
-                   "    cts.fk_ctsLength AS ctsLength,\n" +
-                   "    cts.fk_cladding_id AS ctsCladding_id \n" +
-                   "FROM (((ccp_inquiries\n" +
-                   "    INNER JOIN ccp_inquiry_statuses ON ccp_inquiries.fk_status_id = ccp_inquiry_statuses.status_id)\n" +
-                   "    INNER JOIN ccp ON ccp_inquiries.fk_ccp_id = ccp.ccp_id)\n" +
-                   "    LEFT JOIN cts ON ccp.fk_cts_id = cts.cts_id)\n" +
-                   "WHERE custom_carport_inquiry_id = ?";
+                   "SELECT custom_carport_inquiry_id AS inquiry_id,\n" +
+                          "       inquiryDate, ccp_inquiry_statuses.status AS status,\n" +
+                          "       ccp.ccp_id AS ccp_id, ccp.fk_ccpWidth AS ccpWidth,\n" +
+                          "       ccp.fk_ccpLength AS ccpLength,\n" +
+                          "       ccp.fk_ccpHeight AS ccpHeight,\n" +
+                          "       ccp.fk_cts_id AS cts_id,\n" +
+                          "       cts.fk_ctsWidth AS ctsWidth,\n" +
+                          "       cts.fk_ctsLength AS ctsLength, \n" +
+                          "       cts.fk_cladding_id AS ctsCladding_id \n" +
+                          "FROM (((ccp_inquiries\n" +
+                          "   INNER JOIN ccp_inquiry_statuses ON ccp_inquiries.fk_status_id = ccp_inquiry_statuses.status_id)\n" +
+                          "   INNER JOIN ccp ON ccp_inquiries.fk_ccp_id = ccp.ccp_id)\n" +
+                          "   LEFT JOIN cts ON ccp.fk_cts_id = cts.cts_id)\n" +
+                          "WHERE custom_carport_inquiry_id = ?";
 
             try (PreparedStatement ps = connection.prepareStatement(sql)) {
-                ps.setString(1, searchInput);
+                ps.setInt(1, searchInput);
 
                 ResultSet rs = ps.executeQuery();
 
@@ -323,7 +311,7 @@ public class CustomCarportMapper {
                         // CTS data
                         Toolshed toolshed = null;
 
-                        if(rs.getInt("cts_id") != 0){
+                        if (rs.getInt("cts_id") != 0) {
                             int cts_id = rs.getInt("cts_id");
                             int ctsWidth = rs.getInt("ctsWidth");
                             int ctsLength = rs.getInt("ctsLength");
@@ -331,66 +319,36 @@ public class CustomCarportMapper {
                             toolshed = new Toolshed(cts_id, ctsWidth, ctsLength, ctsCladding_id);
                         }
 
+                        // CCP data
+                        int ccp_id = rs.getInt("ccp_id");
+                        int ccpWidth = rs.getInt("ccpWidth");
+                        int ccpLength = rs.getInt("ccpLength");
+                        int ccpHeight = rs.getInt("ccpHeight");
+
+                        CustomCarport customCarport = new CustomCarport(ccp_id, ccpWidth, ccpLength, ccpHeight, toolshed);
+
                         // Inquiry data
                         int inquiry_id = rs.getInt("inquiry_id");
                         LocalDate inquiryDate = rs.getDate("inquiryDate").toLocalDate();
                         String inquiryStatus = rs.getString("status");
 
-                    int ccp_id = rs.getInt("ccp_id");
-                    int ccpWidth = rs.getInt("ccpWidth");
-                    int ccpLength = rs.getInt("ccpLength");
-                    int ccpHeight = rs.getInt("ccpHeight");
-                    boolean middlePilar = rs.getBoolean("middlePillar");
-                    int rafterSpacing = rs.getInt("rafterSpacing");
-                    int roofType_id = rs.getInt("roofType_id");
-                    int roofAngle = rs.getInt("roofAngle");
-                    int roofMaterial_id = rs.getInt("roofMaterial_id");
-                    double price = rs.getDouble("price");
-
-                    CustomCarport customCarport = new CustomCarport(
-                           ccp_id, ccpWidth, ccpLength, ccpHeight, middlePilar, rafterSpacing, roofType_id, roofAngle,
-                           roofMaterial_id, toolshed, price);
-
-                    // Contact info
-                    ContactInfo contactInfo = null;
-
-                    String firstName = rs.getString("firstName");
-                    String lastName = rs.getString("lastName");
-                    String email = rs.getString("email");
-                    String phoneNum = rs.getString("phoneNum");
-                    String address = rs.getString("address");
-                    int postalcode = rs.getInt("postalcode");
-                    String city = rs.getString("city");
-
-                    contactInfo = new ContactInfo(firstName, lastName, address, postalcode, city, email, phoneNum);
-
-                    // Inquiries
-                    int inquiry_id = rs.getInt("inquiry_id");
-                    LocalDate inquiryDate = rs.getDate("inquiryDate").toLocalDate();
-                    String inquiryStatus = rs.getString("status");
-                    String note = rs.getString("note");
-
-
                         CustomCarportInquiry inquiry = new CustomCarportInquiry(inquiry_id, inquiryDate, inquiryStatus, customCarport);
 
-
+                        inquiries.add(inquiry);
                     } while (rs.next());
 
-                    return inquiry;
-                }else{
-                    throw new UserException("Der var desv. Ingen forespørgsler som matchede det søgte id nummer ");
-
-                    return customCarportInquiry;
+                    return inquiries;
                 } else {
-                    throw new UserException("No corresponding inquiry was found in the database...");
-
+                    throw new UserException("Der var desv. ingen forespørgsler som matchede det søgte id nummer ");
                 }
             } catch (SQLException ex) {
-                throw new UserException("Connection to database could not be established");
+                throw new UserException(ex.getMessage());
             }
 
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException ex) {
+            throw new UserException("Could not establish connection to our database at the moment...");
         }
+
     }
+}
 
--- fog_web/src/main/java/web/commands/SearchInquiryCommand.java
+++ fog_web/src/main/java/web/commands/SearchInquiryCommand.java
@@ -10,41 +10,42 @@ import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class SearchInquiryCommand extends CommandUnprotectedPage {
+public class SearchInquiryCommand extends CommandProtectedPage{
     CustomCarportFacade customCarportFacade;
 
-    public SearchInquiryCommand(String pageToShow) {
-        super(pageToShow);
+    public SearchInquiryCommand(String pageToShow, String role) {
+        super(pageToShow, role);
         customCarportFacade = new CustomCarportFacade(database);
     }
 
 
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
+    public String execute(HttpServletRequest request, HttpServletResponse response){
 
         String searchCategory = request.getParameter("searchCategory");
         String search_input = request.getParameter("search_input");
         ArrayList<CustomCarportInquiry> ccpiArr = new ArrayList<>();
-        HttpSession session =   request.getSession();
+        HttpSession session = request.getSession();
 
         try {
-
-            if (searchCategory.equals("ccpiId")) {
+            if(searchCategory.equals("ccpiId")) {
                 int searchId = Integer.parseInt(search_input);
                 customCarportFacade.getSearchInquiryById(searchId);
                 ccpiArr = customCarportFacade.getSearchInquiryById(searchId);
-            }
-            else if (searchCategory.equals("date")) {
+            } else if (searchCategory.equals("date")) {
+
+            } else if (searchCategory.equals("lastName")) {
 
             }
 
-            else if (searchCategory.equals("lastName")) {
-
-            }
+            session.setAttribute("cppiList", ccpiArr);
+            session.setAttribute("error", null);
 
             return REDIRECT_INDICATOR + super.pageToShow;
         } catch (UserException ex) {
-            session.setAttribute("cppiId", ccpiArr);
+            session.setAttribute("ccpiList", ccpiArr);
+            System.out.println("Here");
+            session.setAttribute("error", ex.getMessage());
             return super.pageToShow;
         }
 
